702d27d2bdb87b3af7adc7444c3a2cb4297c5888
Kraftww <704373461@qq.com>
2018-05-24 01:01:24 +0800

    去除注释

--- day_two/python_two0.py
+++ day_two/python_two0.py
@@ -5,7 +5,7 @@ from math import sqrt
 from sys import stdout
 leap = 1
 h = 0
-for m in range(2,201):
+for m in range(2, 201):
     k = int(sqrt(m + 1))
     print (leap)
     for i in range(2,k + 1):
@@ -14,9 +14,9 @@ for m in range(2,201):
             break
     pass
     if (leap == 1):
-        #print ('%-4d' % m)
+        print ('%-4d' % m)
         h += 1
-        #if (h % 10 == 0):
-            #print ('')
+        if (h % 10 == 0):
+            print ('')
     leap = 1
 print ('The total is %d' % h)
